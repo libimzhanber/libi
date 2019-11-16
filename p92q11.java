b6263cbcfd4fa4520faa93ff863838e88df7a912
libimzhanber <54980528+libimzhanber@users.noreply.github.com>
2019-11-16 15:25:50 +0200

    191027
    עמודים 92-97

--- p92q11.java
+++ p92q11.java
@@ -1,33 +1,68 @@
 import jeliot.io.*;
-import java.util.*;
 
-
-
+public class TV {
+    // Attributes ??????
+    private boolean active; // this.active
+    private int Channel; // this.Channel
+    
+    // Constructor ????
+    
+    // Default Constructor ???? ????? ????
+    public TV() {
+        this.active = false;
+        this.Channel = 99; // Default Channle
+    }
+    
+    // Setters & Getters
+    public void setChannel( int Channel ) {
+        this.Channel = Channel;
+    }
+    public int getChannel() {
+        return this.Channel;
+    }
+    public void setActive( boolean active) {
+        this.active = active;
+    }
+    public boolean getActive() {
+        return this.active;
+    }
+    
+    // Methods
+    public void turnOn() {
+        this.active = true;
+      }
+      public void turnOff() {
+          this.active = false;
+      }
+    public void switchActive() {
+        if (this.active == false ) {
+            this.active = true;
+        }
+        else {
+            this.active = false;
+        }
+    }
+} // TV
 
 public class MyClass {
-public static Scanner reader = new Scanner (System.in);
+
     public static void main() {
-       
-            int num1, num2 ;
-            
-           
-                System.out.println ("Please enter 2 numbers");
-                num1 = reader.nextInt ();
-                num2= reader.nextInt ();
-                
-                Couple c1= new Couple (num1, num2);
-                Couple c2 ;
-                
-                
-            if (c1.getNum1() < c1.getNum2()) {
-                    c2= new Couple(c1.getNum1(), c1.getNum2());
-                    }
-                    
-            else{
-                    c2= new Couple (c1.getNum2(), c1.getNum1());
-            }
-            
-            if ((c1.getNum1 () == c2.getNum1()) && (c1.getNum2()== c2.getNum2 ())) {
-                    System.out.println ("same object");
-                    
-                    }
+        TV tv1 = new TV();
+        TV tv2 = new TV();
+        boolean statusTV1;
+        boolean statusTV2;
+
+
+        tv1.turnOn();
+        tv2.turnOn();
+       tv1.setChannel( 50 );
+        tv2.setChannel( 70 );
+        tv1.turnOff();
+        tv2.switchActive();
+        statusTV2 = tv2.getActive();
+        tv2.switchActive();
+        statusTV2 = tv2.getActive();
+        int Channel2 = tv2.getChannel();
+        
+    }
+}
---
+++ p95q13.java
@@ -0,0 +1,19 @@
+import jeliot.io.*;
+
+   { 
+
+        // Your algorithm goes here.
+int score1;
+int score2;
+int endScore;
+int average;
+
+score1=Input.read.Int;
+score2=Input.readInt;
+if (score1<score2){;
+end Score=(score2*0.9)+(score1*0.1);}
+else{
+end  Score=(score1+score2)/2.0;}
+System.outprintln("end score");
+    }
+                                         
